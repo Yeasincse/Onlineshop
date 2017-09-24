9022363e5becf3ddd4559e059d6742ebe78c1cd9
Yeasincse <yeasincse2012@gmail.com>
2017-09-24 12:54:33 +0600

    this added file base.html,product folder list.html,detail.html, and css file added

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from .models import Category, Product
+from .models import Category, Product, Description_dises
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
