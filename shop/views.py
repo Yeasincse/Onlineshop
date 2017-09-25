b7b8e7088d4ed3591bb6f73f7a0defc4c85dbe55
Yeasincse <yeasincse2012@gmail.com>
2017-09-25 10:42:15 +0600

    this shop app modified shop/static/css/base.css modified:   shop/templates/shop/base.html modified:   shop/templates/shop/product/detail.html  shop/views.py

--- shop/views.py
+++ shop/views.py
@@ -17,10 +17,9 @@ def product_list(request, category_slug=None):
 
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product,
-                                id=id,
-                                slug=slug,
-                                available=True)
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                   'shop/product/detail.html',
-                  {'product': product})
+                  {'product': product,
+                   'cart_product_form': cart_product_form})
